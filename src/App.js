234976d0d3a6f2fc5709acd5c158562ea0dce453
Revanth <revanth0212@gmail.com>
2018-03-06 00:19:27 -0600

    Updating example.

--- src/App.js
+++ src/App.js
@@ -26,6 +26,14 @@ class App extends Component {
   render() {
     return (
       <div>
+        <h3> Dont click or move mouse for 30 seconds for the Inactivity Dialog to show up.</h3>
+        <h3>
+          You should be presented with Options to:
+          <ul>
+            <li>stay which will reset the timer</li>
+            <li>leave which will cancel the timer</li>
+          </ul>
+        </h3>
         <MuiThemeProvider>
           <InactivityDialog
             handleSubmit={this.handleInactivityTimeoutSubmit}
