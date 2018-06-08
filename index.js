27822659e4cbba75a8d535e9362a48bbe4c2b6ec
Revanth <revanth0212@gmail.com>
2018-06-07 21:58:38 -0500

    Updates

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _InactivityDialog = require('./InactivityDialog');
+
+var _InactivityDialog2 = _interopRequireDefault(_InactivityDialog);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _InactivityDialog2.default;
